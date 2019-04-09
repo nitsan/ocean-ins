8f3ec99f772236e5bfa4fe95c12021012993696f
nitsan <nitsanzo@gmail.com>
2019-04-09 16:27:37 +0300

    add more insurances

---
+++ src/app/insurance/construction-insurance/construction-insurance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oi-construction-insurance',
+  templateUrl: './construction-insurance.component.html',
+  styleUrls: ['./construction-insurance.component.scss']
+})
+export class ConstructionInsuranceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/insurance/mortgage-insurance/mortgage-insurance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oi-mortgage-insurance',
+  templateUrl: './mortgage-insurance.component.html',
+  styleUrls: ['./mortgage-insurance.component.scss']
+})
+export class MortgageInsuranceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
