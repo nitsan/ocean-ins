4ba7d60a5d7504cea43d035de87c26a1ad6259e4
nitsanzo@gmail.com <adam1803>
2019-10-15 09:50:21 +0300

    add car price offer

--- src/app/price-offers/services/price-offers.service.ts
+++ src/app/price-offers/services/price-offers.service.ts
@@ -1,6 +1,8 @@
-import {Injectable} from '@angular/core';
-import {EmailsService} from '../../shared/emails/emails.service';
-import {PriceOffer} from '../models/price-offer.enum';
+import { Injectable } from '@angular/core';
+import { EmailsService } from '../../shared/emails/emails.service';
+import { PriceOffer } from '../models/price-offer.enum';
+import { SessionStorageService } from '../../core/services/session-storage.service';
+import { FormGroup } from '@angular/forms';
 
 @Injectable()
 export class PriceOffersService {
@@ -10,16 +12,31 @@ export class PriceOffersService {
     this.formData = {};
   }
 
+  public static loadFormData(formStorageKey: string, form: FormGroup) {
+    const formValue = SessionStorageService.LoadFromSessionStorage(formStorageKey);
+    if (formValue) {
+      form.setValue(formValue);
+    }
+  }
+
+  public static buildYearsOptions(): Array<number> {
+    const yearOptions: Array<number> = [];
+    const currentYear: number = new Date().getFullYear();
+    for (let year: number = currentYear; year >= currentYear - 80; year--) {
+      yearOptions.push(year);
+    }
+
+    return yearOptions;
+  }
+
   addFormData(formData: any) {
-    console.log('formData: ', formData);
     this.formData = {
       ...this.formData,
       ...formData
     };
-    console.log('new formData: ', this.formData);
   }
 
-  sendOfferEmail(offerType: PriceOffer ) {
+  sendOfferEmail(offerType: PriceOffer): Promise<boolean> {
     return this.emailsService.sendPriceOfferMail(this.formData, offerType);
   }
 }
