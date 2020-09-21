7760db8a730308b8548888490742f29b5e315849
Nitsan Zohar <nitsan@next-insurance.com>
2020-09-21 14:51:01 +0300

    Update circleci (#7)
    * Update webpack-bundle-analyzer
    
    * Fix client tests
    
    * Change test to ChromeHeadless
    
    * add lint to circleci
    
    * add client test to circleci
    
    Co-authored-by: nitsanzo@gmail.com <adam1803>

--- src/app/price-offers/services/price-offers.service.spec.ts
+++ src/app/price-offers/services/price-offers.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PriceOffersService } from './price-offers.service';
+import {EmailsService} from '../../shared/emails/emails.service';
 
 describe('PriceOffersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: PriceOffersService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        PriceOffersService,
+        {
+          provide: EmailsService,
+          useValue: {}
+        }
+      ]
+    });
+    service = TestBed.inject(PriceOffersService);
+  });
 
   it('should be created', () => {
-    const service: PriceOffersService = TestBed.get(PriceOffersService);
     expect(service).toBeTruthy();
   });
 });
