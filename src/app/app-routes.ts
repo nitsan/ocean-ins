922cd4db31248404ab971b7a51986c7a912488af
Nitsan Zohar <nitsan@next-insurance.com>
2023-10-27 09:43:58 +0300

    Angular 15 (#214)
    * Run ng update @angular/core@15 @angular/cli@15
    
    * Run @angular/cdk@15 @angular/material@15 @angular-eslint/schematics@15
    
    * Run npm i ngx-cookie-service@15 -f
    
    * Fix scss import
    
    * Fix unit tests

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -60,7 +60,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'enabled' })
   ],
   exports: [
     RouterModule
